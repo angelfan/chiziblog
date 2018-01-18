c2a6c9cb7ce0862f95d95f0eacb17626164f6a04
anglefan <angelfan1314@163.com>
2018-01-18 18:45:46 +0800

    init

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20180117133033) do
 
-  create_table "articles", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", id: :serial, force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
@@ -22,10 +25,11 @@ ActiveRecord::Schema.define(version: 20180117133033) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "article_id"
+    t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
+  add_foreign_key "comments", "articles"
 end
